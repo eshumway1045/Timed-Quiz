c20f41a6344c9b524f7fd2951b02b201e01be1eb
Eric Shumway <ericshumway2@mail.weber.edu>
2022-02-19 22:12:59 -0700

    modifications to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,153 +1,52 @@
-const highScorebtn = document.getElementById('high-score')
-const startButton = document.getElementById('start')
-const nextButton = document.getElementById('next')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-btn')
 
-
-let shuffledQuestions, currentQuestionIndex
-
-
-const questions = [
+var questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript?',
-        answers: [
-            { text: '<script>', correct: true },
-            { text: '<javascript>', correct: false },
-            { text: '<js>', correct: false },
-            { text: '<scripting>', correct: false },
-        ]
+        answers: ['<script>','<javascript>','<js>','<scripting>'],
+        correctAnswer: '<script>'
+
     },
     {
         question: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
-        answers: [
-            { text: 'document.getElement("p").innerHTML = "Hello World!"', correct: false },
-            { text: 'document.getElementById("demo").innerHTML = "Hello World!"', correct: true },
-            { text: 'document.getElementByName("p").innerHTML = "Hello World!"', correct: false },
-            { text: '#demo.innerHTML = "Hello World!"', correct: false },
-        ]
+        answers: ['document.getElement("p").innerHTML = "Hello World!"', 'document.getElementById("demo").innerHTML = "Hello World!"','document.getElementByName("p").innerHTML = "Hello World!"','#demo.innerHTML = "Hello World!"'],
+        correctAnswer: 'document.getElementById("demo").innerHTML = "Hello World!"'
     },
     {
         question: 'Where is the correct place to insert a JavaScript?',
-        answers: [
-            { text: 'Both the <head> section and the <body> section are correct', correct: true },
-            { text: 'The <body> section', correct: false },
-            { text: 'The <head> section', correct: false },
-        ]
+        answers: ['Both the <head> section and the <body> section are correct','The <body> section', 'The <head> section'],
+        correctAnswer: 'Both the <head> section and the <body> section are correct'
     },
     {
         question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-        answers: [
-            { text: '<script src="xxx.js">', correct: true },
-            { text: '<script href="xxx.js">', correct: false },
-            { text: '<script name="xxx.js">', correct: false },
-        ]
+        answers: ['<script src="xxx.js">', '<script href="xxx.js">', '<script name="xxx.js">'],
+        correctAnswer: '<script src="xxx.js">'
     },
     {
         question: 'The external JavaScript file must contain the <script> tag.',
-        answers: [
-            { text: 'True', correct: false },
-            { text: 'False', correct: true },
-        ]
+        answers: ['True', 'False'],
+        correctAnswer:'False'
     }
-]
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
+];
 
-function startGame() {
-    console.log('started');
-    startButton.classList.add('hide');
-    highScorebtn.classList.add('hide')
+
+function beginQuiz(){
     timeKeeper();
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();
-
-
+    createQuestion();
 }
 
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+function timeKeeper(duration, display) {
+    var timeInterval  = setInterval(function(){
+        secondsLeft --;
+        timer.textContent = "Time Remaining: " + secondsLeft + " sec";
 
-}
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-            (answerButtonsElement.firstChild)
-    }
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
-    }
-    else {
-        startButton.innerText = 'Finish'
-        startButton.classList.remove('hide')
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    }
-    else {
-        element.classList.add('wrong')
-    }
-}
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-
-}
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
+        if(secondsLeft <= 0 || (questionIndex > totalQuestions - 1 )){
+            result.style.display = "none";
+            quizContent.style.display = "none";
+            viewResults();
+            clearInterval(timeInterval);
+            timer.textContent = "";
         }
     }, 1000);
 }
 
-function timeKeeper() {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#timer');
-    startTimer(fiveMinutes, display);
-}
+startButton.addEventListener('click', beginQuiz);
